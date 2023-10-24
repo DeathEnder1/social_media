b3fbfeb1888befa9ac03ff60563ec66409e1397b
NakiTomori <datham221@gmail.com>
2023-10-24 19:07:09 +0700

    modified:   social_media/settings.py

--- social_media/settings.py
+++ social_media/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'social_media',
         'USER': 'root',
-        'PASSWORD': 'datham221',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
