a62032857f92effa50775e374ba4f736781355e0
MorpheusOQug <qahp955@gmail.com>
2023-11-10 21:10:32 +0700

    Commit

--- social_media/settings.py
+++ social_media/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'social_media',
         'USER': 'root',
-        'PASSWORD': 'quy1412003',
+        'PASSWORD': '18042003',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT=BASE_DIR / 'static/styles'
+STATIC_ROOT=BASE_DIR / 'static/styles/'
 MEDIA_ROOT = BASE_DIR / 'static/images/'
 
 # Default primary key field type
